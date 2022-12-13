6af28806c9fd99384cd65c5b8a4b35034aed3382
unknown <albertobiel00@hotmail.com>
2022-12-13 15:32:20 -0300

    Atualização CSS

--- src/Componentes/Pokemons.jsx
+++ src/Componentes/Pokemons.jsx
@@ -4,10 +4,9 @@ function Pokemons({ dados }) {
 
     var h0 = "";
     var h1 = "";
-<<<<<<< HEAD
-=======
-    
->>>>>>> 3a7a39ee5b2838199eb81addfadf3907a2d4a684
+
+
+
 
     return (
         <>
@@ -16,13 +15,14 @@ function Pokemons({ dados }) {
                     dados.map((e) => {
                         return (
                             <>
-                                <div className="card" key={e.id}>
-                                    <div className="front">
-                                        <p className="nome">{e.name}</p>
-<<<<<<< HEAD
-                                        <p className="atributos3">{e.types[0].type.name}</p>
-                                        {/* <img className="imagem" src={e.sprites.front_default} alt={e.name} ></img> */}
-                                        <img className='imagem' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${e.id}.svg`} alt={e.id} />
+                                <div className="cards" key={e.id}>
+                                    <div className="card">
+                                        {/* <p className="nome">{e.name}</p>
+                                        <p className="atributos3">{e.types[0].type.name}</p> */}
+                                        <div className='imagem'>
+                                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${e.id}.svg`} alt={e.id} />
+                                        </div>
+
                                         {
                                             e.abilities.forEach((poke, index) => {
                                                 if (index == 0) {
@@ -34,52 +34,16 @@ function Pokemons({ dados }) {
 
                                             })
                                         }
-                                        <p className="habilidade">{h0}</p>
-                                        <p className="habilidade1">{h1}</p>
-=======
-                                        <p className="habilidade">{h0}</p>
-                                        <p className="h1">{h1}</p>
-                                        <p className="atributos3">{e.types[0].type.name}</p>
-                                        {/* <img className="imagem" src={e.sprites.front_default} alt={e.name} ></img> */}
-                                        <img className='imagem' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${e.id}.svg`} alt={e.id} />
-                                        {/* {
-                                            e.abilities.map((poke, index) => {
-                                                return (
-                                                    <>
-                                                        <ol>
-                                                            <li className='habilidade'>
-                                                                {poke.ability.name}
-
-                                                                
-
-                                                            </li>
-                                                        </ol>
-                                                    </>
-                                                )
-                                            })
-                                        }  */}
-
-                                        {
-                                            e.abilities.forEach((poke, index) => {
-                                                if (index == 0 ) {
-                                                    h0 = poke.ability.name;
-
-                                                }if (index == 1) {
-                                                    h1= poke.ability.name
-                                                }
-                                                
-                                            })
-                                        }
->>>>>>> 3a7a39ee5b2838199eb81addfadf3907a2d4a684
-
-                                    </div>
-                                </div>
+                                        {/* <p className="habilidade">{h0}</p>
+                                        <p className="habilidade1">{h1}</p> */}
+                                    </div >
+                                </div >
                             </>
                         )
                     })
                 }
 
-            </div>
+            </div >
         </>
     )
 }
