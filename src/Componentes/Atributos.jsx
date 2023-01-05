cc14c66be88247d4e921dfc4c6054153659bd2c4
unknown <albertobiel00@hotmail.com>
2023-01-05 16:00:48 -0300

    att js

--- src/Componentes/Atributos.jsx
+++ src/Componentes/Atributos.jsx
@@ -1,17 +1,35 @@
-const Atributos = (habilidades) =>{
+import axios from 'axios';
+import './Pokedex.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function Atributos({habilidades}){
+   
     return(
         <>
-            {/* {
+        <p>nha</p>
+            {
                 habilidades.map((a)=>{
+                    
                     return(
                         <>
-
+                           {
+                            a.stats.map((habi)=>{
+                               return(
+                                   <>
+                                   <p className='habi'>{ habi.stat.name}</p>
+                                </>
+                               ) 
+                            })
+                            
+                           }
                         </>
                     )
                 })
-            } */}
+            }
         </>
     )
 }
+
 
 export default Atributos;
