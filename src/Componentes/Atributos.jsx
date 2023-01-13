99aa2e097c36f2fc77628f75678d248882862798
brasi16 <albertobiel00@hotmail.com>
2023-01-12 21:49:29 -0300

    att js e tables

--- src/Componentes/Atributos.jsx
+++ src/Componentes/Atributos.jsx
@@ -2,24 +2,69 @@ import axios from 'axios';
 import './Atributos.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+// import styled, { keyframes } from "styled-components";
 function Atributos({ habilidades }) {
+
+
 
     return (
         <>
-              
+
             {
 
-                
+
                 habilidades.map((a) => {
 
                     return (
                         <>
-                            <div className='habi-numeros'>
-                            <span>HP</span>
-                            <span className='cor' size={`${a.stats[0].base_stat}px`}></span>
+                            {/* <div className='nome-skills'>
+
+                            </div> */}
+
+                            <div className=''>
+                                <table>
+
+                                    <tbody>
+                                        <tr>
+                                            <td> HP</td>
+
+                                            <td>
+                                                <span>{a.stats[0].base_stat}</span>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Attack</td>
+                                            <td>
+                                                <span>{a.stats[1].base_stat}</span>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>SP. Attack</td>
+                                            <td>
+                                                <span>{a.stats[2].base_stat}</span>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>SP. Defense</td>
+                                            <td>
+                                                <span>{a.stats[3].base_stat}</span>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Speed</td>
+                                            <td>
+                                                <span>{a.stats[4].base_stat}</span>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+
+                                </table>
+
                             </div>
-                            <span>{a.stats[0].base_stat}</span>
+                            <br />
                             {/* <div className='card-habilidade'>
                                 {
                                     a.stats.map((habi) => {
@@ -45,7 +90,7 @@ function Atributos({ habilidades }) {
                     )
                 })
             }
-            
+
         </>
     )
 }
